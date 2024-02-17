8b5cb84edb97c5923f4597b14976a5b2f0722425
Piyush-Pant2023 <149172283+Piyush-Pant2023@users.noreply.github.com>
2024-02-17 17:55:14 +0530

    Add files via upload

---
+++ New folder (4)/practice.js
@@ -0,0 +1,22 @@
+    num=5
+    fact=1
+    for(i=1;i<=num;i++){
+        fact=fact*i
+    }
+    console.log(fact)
+     num=5
+     p=3
+     r=1
+    for(i=1;i<=p;i++){
+        r=r*num;
+    }
+    console.log(r)
+    n1=9
+    n2=18
+    for(i=0;i<n1&&n2;i++){
+        if(n1%i==0 && n2%i==0){
+            hcf=i
+        }
+    }   
+        console.log(hcf)
+    
